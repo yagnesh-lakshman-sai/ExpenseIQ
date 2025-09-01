6db1438da69abc3ee06a39f07d8c0c4c0efde9f1
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-09-01 08:30:04 +0530

    added CategoryDTO

---
+++ src/main/java/com/sea/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.sea.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+	private Long id;
+	private String name;
+}
