8430366c29e400ecc68a369cbe6a15c30f4f85f1
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-05-09 10:53:11 +0530

    add AppConstants Class

---
+++ src/main/java/com/sea/util/AppConstants.java
@@ -0,0 +1,10 @@
+package com.sea.util;
+
+public class AppConstants {
+
+    public static final String USER_NOT_FOUND = "User not found";
+
+    public static final String CATEGORY_NOT_FOUND = "Category not found";
+
+    public static final String EXPENSE_NOT_FOUND = "Expense not found";
+}
